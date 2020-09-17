98667230771965265c2bb5aa5781fc6f2987842d
Santi Monjeau <smonjeau@itba.edu.ar>
2020-09-17 00:53:25 -0300

    siguiendo bitmap (ver descrip)
    le meti mascaras, ya pone 1 si ocupa el lugar. Pasan cosas raras con el ceil (math.h) y obvio q falta chequear

--- Kernel/mem_manager/mem_manager.c
+++ Kernel/mem_manager/mem_manager.c
@@ -1,46 +1,56 @@
-#define MEM_SIZE 10*1024*1024
-#define BLOCK_SIZE 1024
-#define BITMAP_SIZE (MEM_SIZE/BLOCK_SIZE)/8
+#define BYTE_SIZE 8
+#define MEM_SIZE 256
+#define BLOCK_SIZE 8
+#define BITMAP_SIZE (MEM_SIZE/BLOCK_SIZE)/BYTE_SIZE
+//#include <stdio.h>
+//#include <math.h>
 
 char memory[MEM_SIZE] = {0};
 
 char bitmap[BITMAP_SIZE] = {0};
 
-void *malloc(int size){
+void *malloco(int size){
 
     int nblocks = ceil(size / BLOCK_SIZE);
-
+    char masks[] = {0x80,0x40,0x20,0x10,0x8,0x4,0x2,0x1};
     int freeBlocks = 0; void *address = memory;
+    void * candidate_adrdress=NULL;
+    int i,j;
 
-    for(int i=0; freeBlocks<nblocks && i<BITMAP_SIZE; i+=1){
+    for( i=0; freeBlocks<nblocks && i<BITMAP_SIZE; i+=1){
 
         char blocksByte = bitmap[i];
 
         // Count free contiguous blocks in this byte
-        
-        for(int j=0; freeBlocks<nblocks && j<8; j++){  
 
-            if((blocksByte >>= 1) % 2 == 0){
-                freeBlocks += 1;
+        for(j=0; freeBlocks<nblocks && j<BYTE_SIZE; j++,address++){
+
+            if(((blocksByte & masks[j] ) / (int)pow(2,BYTE_SIZE-1-j))  % 2 == 0){
+                freeBlocks++;
+                candidate_adrdress = address;
+
             }else{
-                freeBlocks = 0;
-                address = memory + (i*8+j+1)*BLOCK_SIZE;
+                candidate_adrdress =NULL;
+                freeBlocks=0;
             }
-            
         }
-        
+
     }
 
-    if(freeBlocks == nblocks){
-        // Allocate
-    }else{
-        // No space
+
+
+    if(freeBlocks == nblocks) {
+        int k=0,l;
+        while (k<nblocks){
+            for(l=j;k<nblocks && l<BYTE_SIZE;l++){
+                //vamos poniendo unos
+                bitmap[i] |= masks[l];
+                k++;
+            }
+            j=0;
+            i++;
+        }
     }
+    return  candidate_adrdress;
 
 }
-
-// 00011000 00000000
-// address = memory
-// blockByte = 00000000
-// free = 3
-// i = 1, j = 1
