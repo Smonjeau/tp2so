27e5fad2c9bb510481abf750ee94e6233c57d3e0
Bruno Volcovinsky <brunovolko@gmail.com>
2020-09-30 17:13:30 -0300

    Kill funciona + correciones de timer tick

--- Kernel/process_manager/process_manager.s
+++ Kernel/process_manager/process_manager.s
@@ -43,42 +43,6 @@ switchProcessContext:
 	push retAddress
 	ret
 
-createProcessContext:
-	pop retAddress
-
-	# Reserve 1kb for previous process stack growth
-	  sub rsp, 1024
-    
-
-    # Interrupt data
-    push 0      # SS
-    push rbp    # RSP
-    push 0x202  # RFLAGS
-    push 0x8    # CS
-    push rdx 	# RIP	rsp+15*8
-
-    # State data
-    push 0      # r15
-	push 0      # r14
-	push 0      # r13
-	push 0      # r12
-	push 0      # r11
-	push 0      # r10
-	push 0      # r9
-	push 0      # r8
-	push rbp    # rbp
-	push rdi    # rdi (argc)
-	push rsi    # rsi (argv)
-	push rdx    # rdx
-	push rcx    # rcx
-	push rbx    # rbx
-	push rax    # rax
-
-	mov rdi, rsp
-	call createProcessPCB
-
-	push retAddress
-	ret
 
 createProcessContext:
 	cli
@@ -132,6 +96,61 @@ createProcessContext:
 	call createProcessPCB
 
 	sti
+	ret
+
+
+
+createProcessContextFromKernel:
+	
+
+	# Preserve calling args
+	push rdi
+	push rsi
+	push rdx
+
+	# Reserve 1kb for process stack
+	mov rdi, 1024
+	call malloc
+
+	# Restore calling args
+	pop rdx
+	pop rsi
+	pop rdi
+
+	# Calc the base of the stack
+	add rax, 1024
+
+    # Interrupt data
+    movq [rax-8*0], 0     	# SS
+    movq [rax-8*1], rax    	# RSP
+    movq [rax-8*2], 0x202  	# RFLAGS
+    movq [rax-8*3], 0x8    	# CS
+    movq [rax-8*4], rdx 	# RIP	rsp+15*8
+
+    # State data
+    movq [rax-8*5], 1      # r15
+	movq [rax-8*6], 2    	# r14
+	movq [rax-8*7], 3    	# r13
+	movq [rax-8*8], 4    	# r12
+	movq [rax-8*9], 5    	# r11
+	movq [rax-8*10], 6    	# r10
+	movq [rax-8*11], 7   	# r9
+	movq [rax-8*12], 8     	# r8
+	movq [rax-8*13], rax   	# rbp
+	movq [rax-8*14], rdi   	# rdi (argc)
+	movq [rax-8*15], rsi   	# rsi (argv)
+	movq [rax-8*16], 3    	# rdx
+	movq [rax-8*17], 2    	# rcx
+	movq [rax-8*18], 1    	# rbx
+	movq [rax-8*19], 0    	# rax
+
+	# Calc the top of the stack
+	sub rax, 8*19
+	
+	# Create the process PCB
+	mov rdi, rax
+	call createProcessPCBFromKernel
+
 	ret
 
 .section .data
