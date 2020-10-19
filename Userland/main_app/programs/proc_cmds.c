788041fdf788f0e7d7e677d1231f3f93f61da463
Bruno Volcovinsky <brunovolko@gmail.com>
2020-10-19 15:34:46 -0300

    Funciones para pipear procesos fg

--- Userland/main_app/programs/proc_cmds.c
+++ Userland/main_app/programs/proc_cmds.c
@@ -111,3 +111,68 @@ void printSemStatus(){
     free(buffer);
 
 }
+
+void pipeLeftProcMediator(int argc, char * * argv) {
+    int fds[2];
+    fds[0] = atoi(argv[0]);
+    fds[1] = atoi(argv[1]);
+    char * name = argv[2];
+    void (* main) (int, char **) = (void (*) (int, char **)) argv[3];
+
+    close(1); //Solo leo de stdin
+    close(fds[0]); //Solo escribo en el pipe
+    dup(fds[1]); //La escritura del pipe se pone en lugar del stdout
+
+    startProcess(main, 4, arg_vals, "left_proc_mediator", 1); //Ojo que despues hay que cambiar 0 por 1 ////////////////////////////////////
+
+
+    kill(-1); //El kill me hace los close de todas formas
+}
+void pipeRightProcMediator(int argc, char * * argv) {
+    int fds[2];
+    fds[0] = atoi(argv[0]);
+    fds[1] = atoi(argv[1]);
+    char * name = argv[2];
+    void (* main) (int, char **) = (void (*) (int, char **)) argv[3];
+
+    close(0); //Solo escribo a stdout
+    close(fds[1]); //Solo leo del pipe
+    dup(fds[0]); //La lectura del pipe se pone en lugar del stdin
+
+    startProcess(main, 4, arg_vals, "right_proc_mediator", 1); //Ojo que despues hay que cambiar 0 por 1 ////////////////////////////////////
+
+
+    kill(-1); //El kill me hace los close de todas formas
+}
+
+/* Antes de llamar a esta serie de funciones se debe crear pipe y enviar fds por aca*/
+/* Al finalizar debe cerrar fds*/
+void pipeLeftProc(void (* main) (int, char **), char * name, int fds[2]) {
+    //POR AHORA SOLO FOREGRAUND
+    //Todavía soy la shell
+    char fd1[3]; //buffer para guardar fds[0] como string
+    char fd2[3]; //buffer para guardar fds[1] como string
+    itoa(fds[0], fd1, 10, -1);
+    itoa(fds[1], fd2, 10, -1);
+    char * arg_vals[2] = {fd1, fd2, name, (char (*) (int, char **)) main};
+    startProcess(main, 4, arg_vals, "left_proc_mediator", 1); //Ojo que despues hay que cambiar 0 por 1 ////////////////////////////////////
+
+    //La shell se bloquea hasta que el proceso de la izquierda termina de escribir y hace exit
+}
+void pipeRightProc(void (* main) (int, char **), char * name, int fds[2]) {
+    //POR AHORA SOLO FOREGRAUND
+    //Todavía soy la shell
+    char fd1[3]; //buffer para guardar fds[0] como string
+    char fd2[3]; //buffer para guardar fds[1] como string
+    itoa(fds[0], fd1, 10, -1);
+    itoa(fds[1], fd2, 10, -1);
+    char * arg_vals[2] = {fd1, fd2, name, (char (*) (int, char **)) main};
+    startProcess(main, 4, arg_vals, "right_proc_mediator", 1); //Ojo que despues hay que cambiar 0 por 1 ////////////////////////////////////
+
+    //La shell se bloquea hasta que el proceso de la izquierda termina de escribir y hace exit
+}
+void pipeLeftProc(void (* main) (int, char **)) {
+    //POR AHORA SOLO FOREGRAUND    
+}
+
+int startProcess(void *main, int argc, char **argv, char * name, int bg);
