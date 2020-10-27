38b529e43736ada3d68f398559de6fae38e804d9
Matias Julian Apablaza <mapablaza@itba.edu.ar>
2020-10-27 01:03:13 -0300

    Fixed the problem with process names in ps

--- Userland/main_app/programs/shell.c
+++ Userland/main_app/programs/shell.c
@@ -157,12 +157,17 @@ void parseCommand(char *cmdBuff) {
 }
 
 
-void createProcessExec(void (*main)(int,char**), int argc, char **argv, int fg, ProcessExec *p){
+void createProcessExec(void (*main)(int,char**), int argc, char *name, char **argv, int fg, ProcessExec *p){
+
 	p->main = main;
 	p->argc = argc;
 	p->foreground = fg;
 
-	for(int i=0; i<argc; i++)
+	// For the name we will use the given memory zone because the PCB stores a pointer to the name, not a copy
+	// so we cant always use the same location. By passing a pointer to a literal, the problem is solved.
+	p->argv[0] = name;
+
+	for(int i=1; i<argc; i++)
 		memcpy(p->argv[i], argv[i], MAX_TOKEN_LEN);
 
 }
@@ -193,113 +198,113 @@ void parseSimpleCommand(char *cmdBuff, ProcessExec *processExec){
 	// Miscellaneous
 
 	if (strncmp(tokens[0], "help", 5) == 0 && j==1)
-		createProcessExec(printHelp, 1, tokens, fg, processExec);
+		createProcessExec(printHelp, 1, "help", tokens, fg, processExec);
 
 	else if (strncmp(tokens[0], "divzero", 8) == 0 && j==1)
-		createProcessExec(divZeroException, 1, tokens, fg, processExec);
+		createProcessExec(divZeroException, 1, "divzero", tokens, fg, processExec);
 
 	else if (strncmp(tokens[0], "invopcode", 10) == 0 && j==1)
-		createProcessExec(invOpcodeException, 1, tokens, fg, processExec);
+		createProcessExec(invOpcodeException, 1, "invopcode", tokens, fg, processExec);
 
 	else if (strncmp(tokens[0], "time", 5) == 0 && j==1)
-		createProcessExec(printTime, 1, tokens, fg, processExec);
+		createProcessExec(printTime, 1, "time", tokens, fg, processExec);
 
 	else if(strncmp(tokens[0], "display", 8) == 0 && j==2)
-		createProcessExec(displayImage, 2, tokens, fg, processExec);
+		createProcessExec(displayImage, 2, "display", tokens, fg, processExec);
 
 
 	else if(strncmp(tokens[0], "filter", 7) == 0 && j == 1)
-		createProcessExec(filter, 1, tokens, fg, processExec);
+		createProcessExec(filter, 1, "filter", tokens, fg, processExec);
 
 	else if(strncmp(tokens[0], "cat", 4) == 0 && j==1)
-		createProcessExec(cat, 1, tokens, fg, processExec);
+		createProcessExec(cat, 1, "cat", tokens, fg, processExec);
 	
 	else if(strncmp(tokens[0], "wc", 3) == 0 && j==1)
-		createProcessExec(wc, 1, tokens, fg, processExec);
+		createProcessExec(wc, 1, "wc", tokens, fg, processExec);
 
 
 	// CPU management
 
 	else if (strncmp(tokens[0], "cputemp", 8) == 0 && j==1)
-		createProcessExec(printCPUTemp, 1, tokens, fg, processExec);
+		createProcessExec(printCPUTemp, 1, "cputemp", tokens, fg, processExec);
 
 	else if (strncmp(tokens[0], "cpuinfo", 8) == 0 && j==1)
-		createProcessExec(printCPUInfo, 1, tokens, fg, processExec);
+		createProcessExec(printCPUInfo, 1, "cpuinfo", tokens, fg, processExec);
 
 	else if (strncmp(tokens[0], "inforeg", 8) == 0 && j==1)
-		createProcessExec(printInfoReg, 1, tokens, fg, processExec);
+		createProcessExec(printInfoReg, 1, "inforeg", tokens, fg, processExec);
 
 	else if (strncmp(tokens[0], "storedreg", 10) == 0 && j==1)
-		createProcessExec(printStoredReg, 1, tokens, fg, processExec);
+		createProcessExec(printStoredReg, 1, "storedreg", tokens, fg, processExec);
 
 
 	// Memory management
 
 	else if (strncmp(tokens[0], "memdump", 8) == 0 && j == 2)
-		createProcessExec(printMemDump, 2, tokens, fg, processExec);
+		createProcessExec(printMemDump, 2, "memdump", tokens, fg, processExec);
 
 	else if (strncmp(tokens[0], "mem", 4) == 0 && j==1)
-		createProcessExec(printMemStatus, 1, tokens, fg, processExec);
+		createProcessExec(printMemStatus, 1, "mem", tokens, fg, processExec);
 
 
 	// Process management
 
 	else if (strncmp(tokens[0], "kill", 5) == 0 && j == 2)
-		createProcessExec(killProcess, 2, tokens, fg, processExec);
+		createProcessExec(killProcess, 2, "kill", tokens, fg, processExec);
 
 	else if (strncmp(tokens[0], "block", 6) == 0 && j == 2)
-		createProcessExec(blockProcess, 2, tokens, fg, processExec);
+		createProcessExec(blockProcess, 2, "block", tokens, fg, processExec);
 
 	else if (strncmp(tokens[0], "nice", 5) == 0 && j == 3)
-		createProcessExec(niceProcess, 3, tokens, fg, processExec);
+		createProcessExec(niceProcess, 3, "nice", tokens, fg, processExec);
 
 	else if(strncmp(tokens[0], "ps", 3)==0 && j==1)
-		createProcessExec(printProcData, 2, tokens, fg, processExec);
+		createProcessExec(printProcData, 2, "ps", tokens, fg, processExec);
 
 
 	// New processes
 
 	else if(strncmp(tokens[0], "line", 5) == 0 && j==1)	
-		createProcessExec(line, 1, tokens, fg, processExec);
+		createProcessExec(line, 1, "line", tokens, fg, processExec);
 
 	else if (strncmp(tokens[0], "loop", 5) == 0 && j==1)
-		createProcessExec(loop, 1, tokens, fg, processExec);
+		createProcessExec(loop, 1, "loop", tokens, fg, processExec);
 
-	else if(strncmp(tokens[0],"phyloproblem", 13)==0 && j==1)
-		createProcessExec(phylo, 1, tokens, fg, processExec);
+	else if(strncmp(tokens[0], "phyloproblem", 13)==0 && j==1)
+		createProcessExec(phylo, 1, "phyloproblem", tokens, fg, processExec);
 
 
 	//Sync
 
 	else if (strncmp(*tokens, "sem", 4) == 0 && j==1)
-		createProcessExec(printSemStatus, 1, tokens, fg, processExec);
+		createProcessExec(printSemStatus, 1, "sem", tokens, fg, processExec);
 
 	else if(strncmp(tokens[0], "pipe", 5) == 0 && j==1)
-		createProcessExec(printPipeInfo, 1, tokens, fg, processExec);
+		createProcessExec(printPipeInfo, 1, "pipe", tokens, fg, processExec);
 
 
 	//Testing
 
 	else if(strncmp(tokens[0], "testmm", 7) == 0 && j==1)
-		createProcessExec(test_mm, 1, tokens, fg, processExec);
+		createProcessExec(test_mm, 1, "testmm", tokens, fg, processExec);
 
 	else if(strncmp(tokens[0], "testproc", 9) == 0)
-		createProcessExec(test_proc, 1, tokens, fg, processExec);
+		createProcessExec(test_proc, 1, "testproc", tokens, fg, processExec);
 
 	else if(strncmp(tokens[0], "testprio", 9) == 0 && j==1)
-		createProcessExec(test_prio, 1, tokens, fg, processExec);
+		createProcessExec(test_prio, 1, "testprio", tokens, fg, processExec);
 
 	else if(strncmp(tokens[0], "testsync", 9) == 0 && j==1)
-		createProcessExec(test_sync, 1, tokens, fg, processExec);
+		createProcessExec(test_sync, 1, "testsync", tokens, fg, processExec);
 
 	else if(strncmp(tokens[0], "testnosync", 11) == 0 && j==1)
-		createProcessExec(test_no_sync, 1, tokens, fg, processExec);
+		createProcessExec(test_no_sync, 1, "testnosync", tokens, fg, processExec);
 
 	else if(strncmp(tokens[0], "clear", 6) == 0 && j==1)
-		createProcessExec(clearScreen, 1, tokens, fg, processExec);
+		createProcessExec(clearScreen, 1, "clear", tokens, fg, processExec);
 
 	else
-		createProcessExec(printWarning, 1, tokens, fg, processExec);
+		createProcessExec(printWarning, 1, "warning", tokens, fg, processExec);
 	
 }
 
